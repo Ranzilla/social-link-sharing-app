fcfd98b71822de42f8864796f95aa52ff48ebd17
Bitzilla <lucaraneri@hotmail.com>
2023-11-03 16:13:20 +0100

    feat: add components

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,13 @@
+<script>
+    // import svelte animations
+    import { fly} from "svelte/transition";
+    import { page } from "$app/stores";
+</script>
+
+
+<!--key rebuilds all children on change. In this case when url changes.-->
+{#key $page.url}
+    <div in:fly={{  x:'-100%', duration: 500 }}>
+        <slot  />
+    </div>
+{/key}
